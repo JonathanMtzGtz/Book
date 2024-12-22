5ed2faf7f7a4ba2e1badc78ea82407b3b14bbbfb
Jonathan Martinez Gutierrez <61922388+zerodark10@users.noreply.github.com>
2024-12-21 19:05:50 -0600

    Book
    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+Portfolio Jonathan Martinez Gutierrez
+new website v1.0
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/', // Cambia esto si despliegas en un subdirectorio
+});
