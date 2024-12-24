e0764268c2175c9ad5608855ed619e18f2d6a534
Jonathan Martinez Gutierrez <61922388+zerodark10@users.noreply.github.com>
2024-12-24 10:50:13 -0600

    nnew

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // Cambia esto si despliegas en un subdirectorio
+  base: './', // Cambia esto si despliegas en un subdirectorio
 });
