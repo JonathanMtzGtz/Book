19fc00af09e056b276159b35747843b07499cf25
JonathanMtzGtz <martinezxdon@gmail.com>
2025-09-11 22:11:57 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // Directorio raíz (ya dentro de src)
-  publicDir: 'src/public',
+  publicDir: 'public',
 
   build: {
-    outDir: '../dist', // Directorio de salida, fuera de "src"
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: 'index.html',
-        contacto: 'contacto.html',
-        portafolio: 'portafolio.html',
+        main: 'src/index.html',
+        contacto: 'src/contacto.html',
+        portafolio: 'src/portafolio.html',
       },
     },
   },
   optimizeDeps: {
-    include: ['three'] // Asegúrate de incluir dependencias necesarias
+    include: ['three']
   },
 });
